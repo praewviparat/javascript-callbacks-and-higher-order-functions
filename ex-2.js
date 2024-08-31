38032728c8046ae15716a98e93f35b879c608940
EXuanPang <e.xuan@hotmail.com>
2024-08-31 20:27:29 +0900

    feat(ex) : Completed Exercises

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,19 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let num = 0;
+  for(let i = 0; i < array.length; i++){
+    if(operation(array[i])){
+      num++;
+    }
+  }
+   return num >= 5; 
+  }
+
+
+function checkGrade(score){
+  return score > 70;
+
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +23,11 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,checkGrade)
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,checkGrade)
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,checkGrade)
+
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
